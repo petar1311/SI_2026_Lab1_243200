b9d52ea1bd0374f4a6d6542dacf71cef102262d0
Petru <petru@example.com>
2026-03-29 14:26:47 +0200

    Implement borrow book

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -62,7 +62,8 @@ class Library {
             if (book.getTitle().equalsIgnoreCase(title)) {
                 if (!book.isBorrowed()) {
                     book.setBorrowed(true);
-                    System.out.println("Book borrowed.");
+                    // The requirement specifically asks for this message:
+                    System.out.println("Borrowed successfully.");
                 } else {
                     System.out.println("Book is already borrowed.");
                 }
@@ -134,5 +135,7 @@ public class SI2026Lab1Main {
 
         boolean notFound = library.searchBookByTitle("Harry Potter");
         System.out.println("Is 'Harry Potter' in the library? " + notFound);
+
+        library.borrowBook("Clean Code");
     }
 }
